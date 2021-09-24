de7ddc88554b0fb4eaa66e9b6f976afed987b0cb
jarfven <76518936+jarfven@users.noreply.github.com>
2021-09-24 11:06:01 +0200

    Marias portfolio

---
+++ js/maria.js
@@ -0,0 +1,22 @@
+
+
+const images = document.querySelectorAll(".slideImg");
+
+    let i = 0;
+    
+    setInterval(function(){ 
+
+        if(i == 0) {
+          images[i].style.display = 'block';
+        } else if(i == images.length ) {
+          images[i - 1].style.display = 'none';
+          images[0].style.display = 'block';
+          i = 0;
+        } else {
+          images[i - 1].style.display = 'none';
+          images[i].style.display = 'block';
+        }
+        
+       i++;
+       
+      }, 2000);
